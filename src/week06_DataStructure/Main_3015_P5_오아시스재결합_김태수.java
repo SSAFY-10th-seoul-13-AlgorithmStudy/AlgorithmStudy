b2f3b22187fd7a101a7f8a37db1c01dc0eb121d0
benyy0101 <benyy0101@gmail.com>
2023-12-07 09:11:36 +0900

    feat: CI/CD test2

--- src/week06_DataStructure/Main_3015_P5_오아시스재결합_김태수.java
+++ src/week06_DataStructure/Main_3015_P5_오아시스재결합_김태수.java
@@ -1,7 +1,9 @@
 package week06_DataStructure;
 
+import java.io.BufferedReader;
+
 public class Main_3015_P5_오아시스재결합_김태수 {
 	public static void main(String[] args) {
-		
+		BufferedReader br;
 	}
 }
