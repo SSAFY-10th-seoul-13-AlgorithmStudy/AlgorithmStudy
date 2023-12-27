c314dee1bc9ccc5b0e0fc94d8b001ad2835b74a8
heeyeon3050 <111184269+heeyeon3050@users.noreply.github.com>
2023-12-27 14:48:09 +0900

    Main_18427_G4_함께블록쌓기_김희연

---
+++ src/week08_DynamicProgramming/Main_18427_G4_함께블록쌓기_김희연.java
@@ -0,0 +1,40 @@
+package week08_DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_18427_G4_ÇÔ²²ºí·Ï½×±â_±èÈñ¿¬ {
+
+	private static final int MOD = 1000000;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+
+		int[] dp = new int[str.length()+1];
+
+		if(str.charAt(0) == '0'){
+			System.out.println(0);
+			return;
+		}
+
+		dp[0] = 1;
+		dp[1] = 1;
+
+		for(int i=2; i<=str.length(); i++){
+			if(str.charAt(i-1) != '0'){
+				dp[i] += dp[i-1];
+				dp[i] %= MOD;
+			}
+
+			int num = Integer.parseInt(str.substring(i-2, i));
+			if(num >= 10 && num <= 26){
+				dp[i] += dp[i-2];
+				dp[i] %= MOD;
+			}
+		}
+
+		System.out.println(dp[str.length()]);
+	}
+}
