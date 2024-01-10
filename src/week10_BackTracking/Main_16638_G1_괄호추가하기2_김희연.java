7c21f8aec44a9bf4ff4380bc5ce4f93b3231d084
heeyeon3050 <kho200a@naver.com>
2024-01-11 01:23:30 +0900

    Main_16638_G1_괄호추가하기2_김희연 (#44)

--- src/week10_BackTracking/Main_16638_G1_괄호추가하기2_김희연.java
+++ src/week10_BackTracking/Main_16638_G1_괄호추가하기2_김희연.java
@@ -3,110 +3,123 @@ package week10_BackTracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 
 public class Main_16638_G1_괄호추가하기2_김희연 {
 	static int n;
 	static int max = Integer.MIN_VALUE;
-	static String[] arr;
+	static boolean[] brackets;
+	static Formula[] input;
+
+	public static class Formula{
+		char character;
+		int priority;
+
+		Formula(char character, int priority){
+			this.character = character;
+			this.priority = priority;
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		n = Integer.parseInt(br.readLine());
 
-		arr = new String[n];
+		brackets = new boolean[n];
+		input = new Formula[n];
+
 		String str = br.readLine();
-
-		arr = str.split("");
-
-		int cnt = n/2;
-		int target = cnt%2 == 0 ? cnt/2 : (cnt/2)+1;
-
-		calculate(null);
-
-		for(int i=1; i<=target; i++){
-			find(0, 1, i, new int[i]);
+		for(int i=0; i<str.length(); i++){
+			char c = str.charAt(i);
+			if(c == '*')
+				input[i] = new Formula(c, 2);
+			else if(c == '+' || c == '-')
+				input[i] = new Formula(c, 3);
+			else
+				input[i] = new Formula(c, 0);
 		}
+
+		dfs(1);
 
 		System.out.println(max);
 	}
 
-	public static void calculate(int[] select){
-		String[] now;
-		now = arr.clone();
-
-		if(select != null){
-			for(int i=0; i<select.length; i++){
-				if(select[i] != 0){
-					int index = select[i];
-					int front = index - 1;
-					int back = index + 1;
-					int value = 0;
-					switch (now[index]){
-						case "+":
-							value = Integer.parseInt(now[front]) + Integer.parseInt(now[back]);
-							break;
-						case "-":
-							value = Integer.parseInt(now[front]) - Integer.parseInt(now[back]);
-							break;
-						case "*":
-							value = Integer.parseInt(now[front]) * Integer.parseInt(now[back]);
-							break;
-					}
-
-					now[index] = Integer.toString(value);
-					now[front] = now[back] = null;
-				}
-			}
-		}
-
-		for(int i=1; i<n; i++){
-			if(now[i] != null && now[i].equals("*")){
-				int L = i-1, R = i+1;
-				while(L-1 > 0 && now[L] == null){
-					--L;
-				}
-				while(R+1 < n && now[R] == null){
-					++R;
-				}
-				now[i] = Integer.toString(Integer.parseInt(now[L]) * Integer.parseInt(now[R]));
-				now[L] = now[R] = null;
-			}
-		}
-
-		for(int i=1; i<n; i+=2){
-			if(now[i] != null && now[i].equals("*")){
-				int L = i-1, R = i+1;
-				while(L-1 > 0 && now == null)
-					--L;
-				while(R+1 < n && now[R] == null)
-					++R;
-				if(now[i].equals("+")) {
-					now[i] = Integer.toString(Integer.parseInt(now[L]) + Integer.parseInt(now[R]));
-				}else if(now[i].equals("-")) {
-					now[i] = Integer.toString(Integer.parseInt(now[L]) - Integer.parseInt(now[R]));
-				}
-				now[L] = now[R] = null;
-			}
-		}
-		int sum = 0;
-		for (int i = 0; i<n; ++i) {
-			if (now[i] != null)
-				sum += Integer.parseInt(now[i]);
-		}
-		max = Math.max(max, sum);
-	}
-
-	static void find(int cnt, int start, int R, int[] select) {
-		if (cnt == R) {
-			calculate(select);
+	public static void dfs(int depth){
+		if(depth >= n){
+			int result = calculate(infixToPostfix());
+			max = Math.max(max, result);
 			return;
 		}
 
-		for (int i = start; i <n; i += 2) {
-			if (cnt > 0 && (select[cnt - 1] + 2) == i)
-				continue;
-			select[cnt] = i;
-			find(cnt + 1, i + 2, R, select);
+		if(depth == 1){
+			brackets[depth] = true;
+			dfs(depth+2);
+			brackets[depth] = false;
+		} else {
+			if(!brackets[depth-2]){
+				brackets[depth] = true;
+				dfs(depth+2);
+				brackets[depth] = false;
+			}
 		}
+		dfs(depth+2);
+	}
+
+	public static ArrayList<Formula> infixToPostfix(){
+		ArrayList<Formula> postfix = new ArrayList<>();
+		Deque<Formula> deque = new ArrayDeque<>();
+
+		for(int i=0; i<n; i++){
+			if(input[i].priority == 0){
+				postfix.add(input[i]);
+			} else {
+				int priority = input[i].priority;
+				if(brackets[i]){
+					priority = 1;
+				}
+
+				while(!deque.isEmpty() && deque.peek().priority <= priority){
+					postfix.add(deque.pop());
+				}
+
+				deque.push(new Formula(input[i].character, priority));
+			}
+		}
+
+		while(!deque.isEmpty()){
+			postfix.add(deque.pop());
+		}
+
+		return postfix;
+	}
+
+	public static int calculate(ArrayList<Formula> postfix){
+		Deque<Integer> deque = new ArrayDeque<>();
+		int num = 0;
+		for(int i=0; i<n; i++){
+			if(postfix.get(i).priority == 0){
+				deque.push(postfix.get(i).character - '0');
+			} else {
+				int a = deque.pop();
+				int b = deque.pop();
+				char op = postfix.get(i).character;
+				switch (op) {
+					case '+':
+						num = b + a;
+						break;
+					case '-':
+						num = b - a;
+						break;
+					case '*':
+						num = b * a;
+						break;
+				}
+				deque.push(num);
+			}
+		}
+		return deque.pop();
 	}
 }
