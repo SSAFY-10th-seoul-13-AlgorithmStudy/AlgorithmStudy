6de44b66a75728b71ca4edaa2eacaff765e8595b
heeyeon3050 <kho200a@naver.com>
2024-01-29 22:25:34 +0900

    Main_2473_G3_세용액_김희연 (#49)

---
+++ src/week12_BinarySearch/Main_2473_G3_세용액_김희연.java
@@ -0,0 +1,57 @@
+package week12_BinarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_2473_G3_세용액_김희연 {
+	static int[] arr;
+	static int[] liquid = new int[3];
+	static long max = Long.MAX_VALUE;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+
+		arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+
+		for(int i=0; i<n; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		for(int i=0; i<n-2; i++)
+			binarySearch(i); //index, left, right 순일 경우가 마지막이기 때문에 n-3, n-2, n-1 이므로 n-3까지 돌린다.
+
+		for(int i=0; i<3; i++)
+			System.out.print(liquid[i] + " ");
+	}
+
+	public static void binarySearch(int index){
+		int left = index+1;
+		int right = arr.length-1;
+
+		while(left < right){
+			long sum = arr[left] + arr[right] + arr[index];
+			long abs = Math.abs(sum);
+
+			if(abs < max){
+				liquid[0] = arr[index];
+				liquid[1] = arr[left];
+				liquid[2] = arr[right];
+				max = abs;
+			}
+
+			if(sum < 0)
+				left++;
+			else
+				right--;
+		}
+	}
+}
