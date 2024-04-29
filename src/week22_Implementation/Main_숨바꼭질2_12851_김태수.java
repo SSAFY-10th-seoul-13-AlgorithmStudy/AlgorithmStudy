62e5c8d781ac43efca316278c9963804e2fa3a47
benyy0101 <benyy0101@gmail.com>
2024-04-29 22:18:13 +0900

    Main_숨바꼭질2_12851_김태수 (#64)

---
+++ src/week22_Implementation/Main_숨바꼭질2_12851_김태수.java
@@ -0,0 +1,56 @@
+package week22_Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main_숨바꼭질2_12851_김태수 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+	
+		ArrayDeque<Integer> qq = new ArrayDeque<>();
+		
+		qq.add(N);
+		int answer = 0;
+		int[] v = new int[100001];
+		if(N==K) {
+			System.out.println(0);
+			System.out.println(1);
+			return;
+		}
+		
+		while(!qq.isEmpty()) {
+			int cur = qq.poll();
+			
+			if(cur == K ) {
+				answer++;
+				continue;
+			}
+			
+			if(cur+1 >= 0 && cur+1 <= 100_000 && (v[cur+1] == 0 || v[cur+1] >= v[cur]+ 1)) {
+				qq.add(cur+1);
+				v[cur+1] = v[cur] + 1;
+			} 
+			
+			if(cur-1 >= 0 && cur-1 <= 100_000 && (v[cur-1] == 0 || v[cur-1] >= v[cur]+ 1)) {
+				qq.add(cur-1);
+				v[cur-1] = v[cur] + 1;
+			} 
+			if(cur*2 >= 0 && cur*2 <= 100_000 && (v[cur*2] == 0 || v[cur*2] >= v[cur]+ 1)) {
+				qq.add(cur * 2);
+				v[cur*2] = v[cur] + 1;
+			} 
+			
+		}
+		System.out.println(v[K]);
+		System.out.println(answer);
+
+	}
+}
