3996486d9d94ed6d628f02d44819c812c3723197
Ehigh <rkd2rb@naver.com>
2024-05-08 21:55:08 +0900

    Main_1717_G5_집합의표현_강이규 (#70)

---
+++ src/week23_UnionFind/Main_1717_G5_집합의표현_강이규.java
@@ -0,0 +1,53 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main_1717_G5_집합의표현_강이규 {
+
+    private static int N, M;
+    private static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int cmd = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (cmd == 0) union(a, b);
+            else sb.append(find(a) == find(b) ? "YES" : "NO").append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    private static boolean union(int a, int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if (aRoot == bRoot) return false;
+        parent[bRoot] = aRoot;
+        return true;
+    }
+
+    private static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+}
