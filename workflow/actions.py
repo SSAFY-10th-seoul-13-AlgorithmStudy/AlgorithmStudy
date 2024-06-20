7c87b7e6f55dc324d290b14c1624e73396fcd654
benyy0101 <benyy0101@gmail.com>
2024-06-20 21:51:45 +0900

    SYS: actions refactored

--- workflow/actions.py
+++ workflow/actions.py
@@ -5,124 +5,99 @@ users = [
     "강이규",
     "김아린",
     "김태수",
-    "김희연",
+    "신문영",
     "송윤재",
-    "권송아"
 ]
-Algorithm = [
-    'BackTracking',
-    'BFS_DFS',
-    'BinarySearch',
-    'BruteForce',
-    'DataStructure',
-    'Dijkstra',
-    'DivideAndConquer',
-    'DynamicProgramming',
-    'FloydWarshall',
-    'Greedy',
-    'Implementation',
-    'KMP',
-    'LCA',
-    'MST',
-    'PrefixSum',
-    'SegmentTree',
-    'String',
-    'TopologySort',
-    'Tree',
-    'Trie',
-    'TwoPointer',
-    'UnionFind'
+
+algorithms = [
+    'BackTracking', 'BFS_DFS', 'BinarySearch', 'BruteForce', 'DataStructure',
+    'Dijkstra', 'DivideAndConquer', 'DynamicProgramming', 'FloydWarshall', 'Greedy',
+    'Implementation', 'KMP', 'LCA', 'MST', 'PrefixSum', 'SegmentTree', 'String',
+    'TopologySort', 'Tree', 'Trie', 'TwoPointer', 'UnionFind'
 ]
 
 file_path = os.path.abspath(__file__)
 BASE_DIR = os.path.dirname(file_path)
+
 def generateREADME():
-    with open(os.path.join(BASE_DIR,"..","README.md"),"w",encoding="UTF-8") as f:
-        f.write("# ☘️ Algorithm For B ☘️\n") ##제목 쓰세요
-        ## 원하는 본문 내용 있으면 추가해 주세요
+    with open(os.path.join(BASE_DIR, "..", "README.md"), "w", encoding="UTF-8") as f:
+        f.write("# ☘️ Algorithm For B ☘️\n")
         f.write("## 알고리즘별 분류 \n")
-        data = getAlgoData()
-        f.write(createAlgo(data))
+        algo_data = getAlgoData()
+        f.write(createAlgoSection(algo_data))
         f.write("## 주차별 풀이 상황 \n")
         
-        week = getWeekData()
-        for case in sorted(week.keys()):
-            f.write(createWeekToggle(week[case], case))
+        week_data = getWeekData()
+        for week in sorted(week_data.keys()):
+            f.write(createWeekSection(week_data[week], week))
         print("all records are up to date!")
 
 def getAlgoData():
-    data = {}
-    for pathName in os.listdir(BASE_DIR+"/../src"):
+    data = defaultdict(lambda: defaultdict(int))
+    src_dir = os.path.join(BASE_DIR, "..", "src")
+    for pathName in os.listdir(src_dir):
         if len(pathName.split("_")) >= 2:
-            algo = ("_").join(pathName.split("_")[1:])
-            # algo = pathName.split("_")[1]
-            #print(algo)
-            if(algo not in data): data[algo] = {}
-            for fileName in os.listdir(os.path.join(BASE_DIR,"..","src",pathName)):
+            algo = "_".join(pathName.split("_")[1:])
+            algo_path = os.path.join(src_dir, pathName)
+            for fileName in os.listdir(algo_path):
                 solver = fileName.split("_")[4].split(".")[0]
-                if solver not in data[algo]: data[algo][solver] = 1
-                else: data[algo][solver] += 1
-    #print("data: " ,data)
+                data[algo][solver] += 1
     return data
-            
 
+def createAlgoSection(data):
+    header = createAlgoHeader()
+    body = createAlgoBody(data)
+    return header + body
 
-def createAlgo(data):
-    #print("create: ",data)
-    line = '''| Algorithm | '''
-    for user in users:
-        line += user + ''' | '''
-    line += '''\n | :--: | :--: | :--: | :--: | :--: | :--: |:--: | \n'''
-    for algo in Algorithm:
-        line += (" | " + algo + " | ")
+def createAlgoHeader():
+    header = "| Algorithm | "
+    header += " | ".join(users) + " | \n"
+    header += "| :--: " * (len(users) + 1) + "|\n"
+    return header
+
+def createAlgoBody(data):
+    body = ""
+    for algo in algorithms:
+        body += f" | {algo} | "
         for user in users:
-            if(algo not in data): continue
-            if(user not in data[algo]): line += " 0 |"
-            else: line += (str(data[algo][user]) + " | ")
-        line += "\n"
-    return line
-    
+            if algo in data and user in data[algo]:
+                body += f"{data[algo][user]} | "
+            else:
+                body += "0 | "
+        body += "\n"
+    return body
 
-def createWeekToggle(data,week):
-        #print(data)
-        line = ""
-        line += '''\n<details>\n<summary>\n'''
-        line += (week)
-        line += '''\n</summary>\n'''
-        line += '''<div markdown="1">\n'''
-        line += "\n"
-        line += " | 문제 | "
+def createWeekSection(data, week):
+    header = createWeekHeader(week)
+    body = createWeekBody(data)
+    return header + body
+
+def createWeekHeader(week):
+    header = f"\n<details>\n<summary>\n{week}\n</summary>\n<div markdown=\"1\">\n\n"
+    header += " | 문제 | " + " | ".join(users) + " | \n"
+    header += "| :--: " * (len(users) + 1) + "|\n"
+    return header
+
+def createWeekBody(data):
+    body = ""
+    for problem in data:
+        body += f" | {problem} | "
         for user in users:
-            line += user
-            line += " | "
-        line += '''\n | :--: | :--: | :--: | :--: | :--: | :--: |:--: | \n'''
-        for problem in data:
-            line += (" | " + problem + " | ")
-            for user in users:
-                #print(user)
-                if user not in data[problem]:
-                    line += ( " ❌ "+ " | ")
-                else:
-                    line += ( " ✔️ "+ " | ")
-            line += "\n"
-        line += '''</div>'''
-        line+='''</details>\n<br>'''
-        return line
-        
+            body += "✔️ | " if user in data[problem] else "❌ | "
+        body += "\n"
+    body += "</div></details>\n<br>"
+    return body
+
 def getWeekData():
-    count="1"
-    data = {}
-    #print(os.listdir("../src"))
-    #os.path.join(BASE_DIR,"..","src",pathName)
-    for pathName in os.listdir(os.path.join(BASE_DIR,"..","src")):
-        data[pathName] = {}
-        print(os.path.join(BASE_DIR,"..","src",pathName))
-        fileList = os.listdir(os.path.join(BASE_DIR,"..","src",pathName))
-            #print ("file_list: {}".format(fileList))
-        for file in fileList:
+    data = defaultdict(lambda: defaultdict(list))
+    src_dir = os.path.join(BASE_DIR, "..", "src")
+    for pathName in os.listdir(src_dir):
+        problem_path = os.path.join(src_dir, pathName)
+        for file in os.listdir(problem_path):
             solve = file.split("_")
-            if(solve[3] not in data[pathName]): data[pathName][solve[3]] = []
-            data[pathName][solve[3]].append(solve[4].split(".")[0])
+            user = solve[3]
+            problem = solve[4].split(".")[0]
+            data[pathName][user].append(problem)
     return data
 
 generateREADME()
